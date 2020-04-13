845a474012ba9c0d300985a11f7d01f127505ca5
vantutrieu97 <vantutrieu97@gmail.com>
2020-04-13 17:18:44 +0700

    Numbers and Operators

---
+++ src/Numbers.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val year = 1997
+    val month = 8
+    val day = 7
+
+    // + - * /
+    println(year + month + day)
+    println(year - month - day)
+    println(year * month * day)
+    println(year / month / day)
+    
+    println(year % month % day)
+}
